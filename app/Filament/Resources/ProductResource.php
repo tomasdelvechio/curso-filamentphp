bf3e6cac0a69c3dfc46012b958748537ea0f6d62
Tomas Delvechio <tomasdelvechio17@gmail.com>
2024-04-23 09:07:05 -0300

    video 12 filtros en listado

--- app/Filament/Resources/ProductResource.php
+++ app/Filament/Resources/ProductResource.php
@@ -23,6 +23,8 @@ use Filament\Tables;
 use Filament\Tables\Columns\IconColumn;
 use Filament\Tables\Columns\ImageColumn;
 use Filament\Tables\Columns\TextColumn;
+use Filament\Tables\Filters\SelectFilter;
+use Filament\Tables\Filters\TernaryFilter;
 use Filament\Tables\Table;
 use Illuminate\Database\Eloquent\Builder;
 use Illuminate\Database\Eloquent\SoftDeletingScope;
@@ -150,6 +152,14 @@ class ProductResource extends Resource
             ])
             ->filters([
                 //
+                TernaryFilter::make('is_visible')
+                    ->label('Visibility')
+                    ->boolean()
+                    ->trueLabel('Only Visible Products')
+                    ->falseLabel('Only Hidden Products')
+                    ->native(false),
+                SelectFilter::make('brand')
+                    ->relationship('brand', 'name')
             ])
             ->actions([
                 Tables\Actions\EditAction::make(),
